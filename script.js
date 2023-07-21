9657dcb6056be2579c2060ead2bb9baf74d364fa
Sadan Mian <sadanmiyan007@gmail.com>
2023-07-21 10:15:11 +0530

    arrow

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 let imgBox = document.querySelector('.img-box')
 let imgWrap = document.querySelector('.img-wrap')
 let originalImg = document.getElementById('originalImg')
+let line = document.getElementById('line')
 
 originalImg.style.width = imgBox.offsetWidth + "px"
 let leftSpace = imgBox.offsetLeft
@@ -8,4 +9,5 @@ let leftSpace = imgBox.offsetLeft
 imgBox.onmousemove = (e) => {
     let boxWidth = (e.pageX - leftSpace) + "px"
     imgWrap.style.width = boxWidth
+    line.style.left = boxWidth
 }
