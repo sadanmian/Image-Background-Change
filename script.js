81bf1fc0a7293471890c906c959291881c3998b3
Sadan Mian <sadanmiyan007@gmail.com>
2023-07-21 10:06:34 +0530

    img wrap

---
+++ script.js
@@ -0,0 +1,11 @@
+let imgBox = document.querySelector('.img-box')
+let imgWrap = document.querySelector('.img-wrap')
+let originalImg = document.getElementById('originalImg')
+
+originalImg.style.width = imgBox.offsetWidth + "px"
+let leftSpace = imgBox.offsetLeft
+
+imgBox.onmousemove = (e) => {
+    let boxWidth = (e.pageX - leftSpace) + "px"
+    imgWrap.style.width = boxWidth
+}
